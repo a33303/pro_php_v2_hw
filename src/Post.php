c67b4377e4254e41e772a33ec27833351eb66f91
a3330 <a33303@mail.ru>
2022-09-08 00:11:33 +0300

    hw1

---
+++ src/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+use src\Person;
+
+class Post
+{
+    public function __construct(
+        private Person $author,
+        private string $text
+    ) {
+    }
+    public function __toString()
+    {
+        return $this->author . ' пишет: ' . $this->text;
+    }
+}
